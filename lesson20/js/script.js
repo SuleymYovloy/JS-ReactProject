9527fc1c129fdd5422aeeda8bc00b33688b5df32
Suleym <suleymevlo81@gmail.com>
2022-03-29 00:55:33 +0300

    Прошел 20 Урок: Объекты. Деструктуризация объектов

---
+++ lesson20/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const options = {
+  name: "test",
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: "black",
+    bg: "red",
+  },
+  makeTest: function () {
+    console.log("Test");
+  },
+};
+
+options.makeTest();
+
+const { border, bg } = options.colors; //Деструктуризация объектов
+console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name; // Удаление
+
+// console.log(options);
+// //
+
+// // перебор всех элементов объекта
+// for (let key in options) {
+//   if (typeof options[key] === "object") {
+//     for (let i in options[key]) {
+//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//     }
+//   } else {
+//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//   }
+//   console.log(`Свойство ${key} имеет значение ${options[key]}`);
+// }
