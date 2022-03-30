80f0ec8e6d21ecc5c59d451cba66ce19894d6ba8
SuleymYovloy <suleymevlo81@gmail.com>
2022-03-30 22:29:18 +0300

    Прошел 23 урок: Основы ООП

---
+++ lesson23/js/script.js
@@ -0,0 +1,27 @@
+"use strict"
+
+let str = "some";
+let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+    health: 400,
+    armor: 100,
+    sayHello: function () {
+        console.log("Hello");
+    }
+}
+
+const john = Object.create(soldier)
+ 
+// const jon = {
+//     health: 100
+// }
+
+// Object.setPrototypeOf(jon, soldier);
+
+jon.sayHello()
