a4ba0e5ea3a57274bf2a5ddb7f6199d664023abe
Suleym <suleymevlo81@gmail.com>
2022-05-20 13:43:21 +0300

    Прошел 46 урок

--- lesson46/js/script.js
+++ lesson46/js/script.js
@@ -1,14 +1,15 @@
 // console.log(document.body);
+// console.log(document.documentElement);
 // console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
 // console.log(document.body.firstElementChild);
+// console.log(document.body.firstChild);
 // console.log(document.body.lastChild);
-// console.log(document.body.lastElementChild);
 
 // console.log(document.querySelector("#current").parentNode.parentNode);
-// console.log(document.querySelector("#current").parentElement);
 
 // console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+// console.log(document.querySelector("#current").parentElement);
 
 for (let node of document.body.childNodes) {
   if (node.nodeName == "#text") {
