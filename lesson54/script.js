63b8f7b9f71e2b8c655e6637783d35087c9c4eb3
Suleym <suleymevlo81@gmail.com>
2022-06-04 11:39:48 +0300

    Lesson54 finish

--- lesson54/script.js
+++ lesson54/script.js
@@ -1,6 +1,27 @@
-'use strict';
+"use strict";
 
-const box = document.querySelector('.box');
-const block = document.querySelector('.block');
+const box = document.querySelector(".box");
+const block = document.querySelector(".block");
 
-console.log(block)
+console.log(block);
+
+// if (block) {
+//   console.log(block.textContent);
+// }
+
+// console.log(block?.textContent);
+
+// console.log(1 + 2);
+
+const userData = {
+  name: "Ivan",
+  age: null,
+  say: function () {
+    console.log("Hello");
+  },
+};
+
+userData.say();
+userData.hay?.();
+
+console.log(userData?.skills?.js);
