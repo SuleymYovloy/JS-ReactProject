7b55d499ca68d12625017fdf09e69b6a2e62484f
Suleym <suleymevlo81@gmail.com>
2022-06-06 10:56:11 +0300

    lesson 56 finisg

---
+++ lesson56/script.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+// let id = Symbol("id");
+
+// const obj = {
+//   name: "Test",
+//   [id]: 1,
+//   getId: function () {
+//     return this[id];
+//   },
+// };
+
+// // let id = Symbol("id");
+
+// // obj[id] = 1;
+
+// console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
+
+// // for (let value in obj) console.log(value);
