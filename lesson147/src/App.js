087c211c001ee754f64e51cb97cd9bf1fad6e2e7
Suleym <suleymevlo81@gmail.com>
2022-09-13 11:24:16 +0300

    create-component-project

--- lesson147/src/App.js
+++ lesson147/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+
+function WhoAmi ({name, surname, link}) {
+  return (
+    <div>
+      <h1>My name is {name}, surname - {surname}</h1>
+      <a href={link}>My profile</a>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {
+        WhoAmi({
+          name: '111',
+          surname: 'David',
+          link: 'instagram.com'
+        })
+      }
+      <WhoAmi name="john" surname="David" link="instagram.com"/>
+      <WhoAmi name="toopack" surname="toop" link="instagram.com"/>
     </div>
   );
 }
 
 export default App;
+
+let props = {
+  name: '111',
+  surname: 'David',   
+  link: 'instagram.com'
+}
+
+console.log(props.name);
